58d2f3602c7ae384178fc5e6a8fcc922ccdfa158
celer7841 <celer7841@wrdsb.ca>
2020-10-06 23:54:38 +0000

    finished

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
  *
- * @author 
+ * @author celer7841
  */
 public class Main {
 
@@ -10,6 +11,17 @@ public class Main {
    */
   public static void main(String[] args) {
     // TODO code application logic here
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Please enter a starting balance");
+    double balance = sc.nextDouble();
+    System.out.println("Please enter your interest rate");
+    int rate = sc.nextInt();
+    System.out.println("Please enter the number of years you will invest for");
+    int years = sc.nextInt();
+    for (int i= 1; i<= years; i++){
+      balance = balance + balance * rate/100;
+    }
+      System.out.println("Your final balance would be $" + balance);
     
   }
 }
